e8e45c2dc2ccd800c7841495d59b717a7ca3ba9d
JScript <janielsonsempresantos@hotmail.com>
2024-03-23 19:59:04 -0300

    terminei a logica do exercicio 4

--- scriptEx04.js
+++ scriptEx04.js
@@ -1,26 +1,36 @@
-const buttomSeletorOperation = document.querySelector('.buttomOperation');
-buttomSeletorOperation.addEventListener('click',operations);
 
+const botao = document.querySelector('.buttomOperation').addEventListener('click' , operations);
 
 function operations() {
     const valueInput1 = parseFloat(document.getElementById('value1').value);
     const valueInput2 = parseFloat(document.getElementById('value2').value);
+  
+    let selectedRadio = document.querySelector('input[name = operation]:checked');
 
-    const inputSeletores = document.querySelectorAll(".selectsRadio");
-
-    let calculation = 0
-
-    for (let i = 0; i < inputSeletores.length; i++) {
-        if (inputSeletores[i].value.checked === 'ADIÇÃO') {
-            calculation = valueInput1 + valueInput2;
-        } else if (inputSeletores[i].checked === 'MULTIPLICAÇÃO') {
-            calculation = valueInput1 * valueInput2;
-        } else if (inputSeletores[i].checked === 'SUBTRAÇÃO') {
-            calculation = valueInput1 - valueInput2;
-        } else if (inputSeletores[i].checked === 'DIVISÃO') {
-            calculation = valueInput1 / valueInput2;
+  
+  
+    let calculation = 0;
+  
+    if (selectedRadio) {
+      let selectedValue = selectedRadio.value;
+  
+      if (selectedValue === 'ADIÇÃO') {
+        calculation = valueInput1 + valueInput2;
+      } else if (selectedValue === 'MULTIPLICAÇÃO') {
+        calculation = valueInput1 * valueInput2;
+      } else if (selectedValue === 'SUBTRAÇÃO') {
+        calculation = valueInput1 - valueInput2;
+        if(calculation < 0) {
+            calculation = calculation * -1 ;
         }
+      } else if (selectedValue === 'DIVISÃO') {
+        calculation = valueInput1 / valueInput2;
+      }
+    } else {
+      // Lidar com o caso em que nenhum botão de rádio está selecionado (opcional)
+      console.log("Por favor, selecione uma operação.");
     }
-    console.log(calculation);
-}
-
+  
+    alert(`o valor calculado e: ${calculation.toFixed(2)}`);
+  }
+  
